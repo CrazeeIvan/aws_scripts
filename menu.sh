ff3b1211de213fb6925cf4f04d62271a313cd07c
Ciarán Maher (Laptop) <ciaran.maher@teamaol.com>
2016-11-14 14:54:17 +0000

    Added more detailed and specific menu feedback on user selection.

--- menu.sh
+++ menu.sh
@@ -14,25 +14,25 @@ select opt in "${options[@]}"
 do
     case $opt in
         "html")
-            echo "you chose choice 1"
+            echo "Starting web server installation and startup"
             curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/html_startup.sh
             chmod 777 html_startup.sh
             ./html_startup.sh
             ;;
         "vpn")
-            echo "you chose choice 2"
+            echo "Starting vpn server installation and startup"
             curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/vpn_startup.sh
             chmod 777 vpn_startup.sh
             ./vpn_startup.sh
             ;;
         "gradle")
-            echo "you chose choice 3"
+            echo "Starting gradle installation and startup"
             curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/gradle_startup.sh
             chmod 777 gradle_startup.sh
             ./gradle_startup.sh
             ;;
         "docker")
-            echo "you chose choice 3"
+            echo "Starting docker installation and startup"
             curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/docker_startup.sh
             chmod 777 docker_startup.sh
             ./docker_startup.sh
