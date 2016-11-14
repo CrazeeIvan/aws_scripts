303153ae2eb4bf114f3b06356c70c2ab764bac13
CrazeeIvan <ciaranmaher@gmail.com>
2016-11-14 01:51:01 +0000

    Added 'permission denied' work around.

--- gradle_startup.sh
+++ gradle_startup.sh
@@ -3,7 +3,8 @@ sudo gradle_version=2.6
 sudo wget -c http://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
 sudo unzip  gradle-${gradle_version}-all.zip -d /opt
 sudo ln -s /opt/gradle-${gradle_version} /opt/gradle
-sudo printf "export GRADLE_HOME=/opt/gradle\nexport PATH=\$PATH:\$GRADLE_HOME/bin\n" > /etc/profile.d/gradle.sh
+sudo printf "export GRADLE_HOME=/opt/gradle\nexport PATH=\$PATH:\$GRADLE_HOME/bin\n" > gradle.sh
+sudo mv -f gradle.sh /etc/profile.d/gradle.#!/bin/sh
 sudo . /etc/profile.d/gradle.sh
 # check installation
 gradle -v
