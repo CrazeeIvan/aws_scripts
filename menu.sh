7a9658d4642e5ae26292cdfdf7b791700aa880dc
Ciarán Maher (Laptop) <ciaran.maher@teamaol.com>
2016-11-14 14:51:26 +0000

    Expanded functionality to pull existing scripts from the menu.

--- menu.sh
+++ menu.sh
@@ -2,31 +2,40 @@
 #curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/menu.sh
 #written by Ciarán Maher 14th/November/2016
 #simple menu for pulling and starting other startup scripts
-
+clear
 echo '############################################'
 echo '##            CIARÁN'S SCRIPTS            ##'
 echo '############################################'
 echo '#  Select a script to download an execute  #'
 echo '############################################'
 PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
+options=("html" "vpn" "gradle" "docker" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
+        "html")
             echo "you chose choice 1"
-            echo "test.test.test"
-            sudo yum update -y
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            sudo yum update -y
-            ;;
-        "Option 3")
-            echo "you chose choice 3"
             curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/html_startup.sh
             chmod 777 html_startup.sh
             ./html_startup.sh
+            ;;
+        "vpn")
+            echo "you chose choice 2"
+            curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/vpn_startup.sh
+            chmod 777 vpn_startup.sh
+            ./vpn_startup.sh
+            ;;
+        "gradle")
+            echo "you chose choice 3"
+            curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/gradle_startup.sh
+            chmod 777 gradle_startup.sh
+            ./gradle_startup.sh
+            ;;
+        "docker")
+            echo "you chose choice 3"
+            curl -O https://raw.githubusercontent.com/CrazeeIvan/aws_scripts/master/docker_startup.sh
+            chmod 777 docker_startup.sh
+            ./docker_startup.sh
             ;;
         "Quit")
             break
